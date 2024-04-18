9c034162a84c2035e43a29623cfd2ba15bc29e78
filvet80 <70665320+filvet80@users.noreply.github.com>
2024-04-18 17:28:51 +0300

    agrasddgvsga

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace HotelSelect
         /// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
+        //fudhjdkskhwqqqqqqqqqqqqqqqqqqqqrqeeeeeeeojiwqrfjwjpdiiohfq
         static void Main()
         {
             Application.EnableVisualStyles();
